2424c4b9d049b3836cdd7899d5697cc7b719a4e2
Fish Kuo <fishkuo0101@gmail.com>
2020-12-09 10:31:27 +0800

    ptt crawler init

---
+++ PTT/get_post.rb
@@ -0,0 +1,12 @@
+# require 'net/http'
+
+
+# u = URI("https://www.ptt.cc/bbs/Gossiping/index.html");
+	
+# http = Net::HTTP.new(u.host,u.port);
+# headers = {
+#     'Cookie'=>'__cfduid=d5936c7500c3f2823830529dff72895d41607441722; _ga=GA1.2.1763610952.1607441736; _gid=GA1.2.398683409.1607441736; over18=1'
+# }
+    
+# doc = http.get(u,headers)
+# p doc
